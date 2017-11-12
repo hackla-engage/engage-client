2017b4142cddea292be514630969aea2cca3b049
Jeff Yamamoto <jeffmagician@gmail.com>
2017-11-11 18:31:36 -0800

    Refactor remove unused comments, component and files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import Landing from './component/Landing.jsx'
-import CounterContainer from './container/CounterContainer.jsx'
 import { configureStore, history} from './store/configureStore'
 import 'babel-polyfill';
-import { Switch } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux';
 import Routes from './routes';
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import App from './container/App.jsx';
 import Landing from './component/Landing.jsx';
 import CounterContainer from './container/CounterContainer.jsx';
 
