76f5d3b13fe26fcf851fc7a3a834c4b98d077915
Jeff Yamamoto <jeffmagician@gmail.com>
2018-01-22 22:59:49 -0800

    Implement menu with semantic ui

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,45 +1,37 @@
 import React, { Component } from 'react';
 import styles from './Landing.scss';
 import { Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react'
 
-class Header extends Component {
-	render() {
-		return (
-			<div>
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
-          <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#">Engage</a>
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              <i className="fa fa-bars"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link js-scroll-trigger" to="/redux">redux</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link js-scroll-trigger" to="/preference">preference</Link>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="#about">about</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="#tutorial">tutorial</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="#contact">contact</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link js-scroll-trigger" href="./signin">signin</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-			</div>
-		)
-	}
+const Header = ()=> {
+  return (
+    <Menu>
+      <Menu.Item name='browse'>
+        <Link to="/">Engage</Link>
+      </Menu.Item>
+
+      <Menu.Menu position='right'>
+        <Menu.Item name='redux'>
+          <Link to="/redux">redux</Link>
+        </Menu.Item>
+        <Menu.Item name='preference'>
+          <Link to="/preference">preference</Link>
+        </Menu.Item>
+        <Menu.Item name='about'>
+          <Link to="/about">about</Link>
+        </Menu.Item>
+        <Menu.Item name='tutorial'>
+          <Link to="/tutorial">tutorial</Link>
+        </Menu.Item>
+        <Menu.Item name='contact'>
+          <Link to="/contact">contact</Link>
+        </Menu.Item>
+        <Menu.Item name='signin'>
+          <Link to="/signin">signin</Link>
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  )
 }
 
 export default Header;
