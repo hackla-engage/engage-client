45dad928ba9f1b1f5098b49bbd4b513b9b8cf126
Evaline <evalinebai@gmail.com>
2018-05-13 10:53:01 -0700

    Remove deprecated redux files for preference

--- src/actions/Preference.js
+++
@@ -1,7 +0,0 @@
-export const preference_selected = preference => {
-    console.log(`%cPreference Selected: ${preference.title} in ${preference.location} `, 'color: #18bc9c');
-    return {
-        type: `PREFERENCE_SELECTED`,
-        payload: preference
-    }
-}
--- src/constants/actionTypes.js
+++
@@ -1,4 +0,0 @@
-/*
- * GET /tags request
- */
-export const REQUEST_TAGS = 'REQUEST_TAGS';
