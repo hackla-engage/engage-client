51b8e864d2bf1aa9ca11486ee98653c54734b10e
Jeff Yamamoto <jeffmagician@gmail.com>
2018-07-08 18:31:13 -0700

    Remove unused code

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,10 @@ import About from './component/About.jsx';
 import Landing from './component/Landing.jsx';
 import Header from './component/Header.jsx';
 import Footer from './component/Footer.jsx';
-import PreferenceList from './container/PreferenceList.jsx';
 import AgendaFeedContainer from './container/AgendaFeedContainer.jsx';
 import SignIn from './component/SignIn.jsx';
 import Faq from './component/Faq.jsx';
 import FormContainer from './container/FormContainer.jsx';
-import TopicsSelectorContainer from './container/TopicsSelectorContainer.jsx';
 
 const Routes = () => {
   return (
@@ -18,12 +16,10 @@ const Routes = () => {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/about" component={About} />
-        <Route path="/preference" component={PreferenceList} />
         <Route path="/feed" component={AgendaFeedContainer} />
         <Route path="/signin" component={SignIn} />
         <Route path="/faq" component={Faq} />
         <Route path="/form" component={FormContainer} />
-        <Route path="/topics" component={TopicsSelectorContainer} />
       </Switch>
       <Route component={Footer} />
     </div>
