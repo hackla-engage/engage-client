e739529ff2eced8d338cee12a34a5edf66076244
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-07-11 03:03:14 -0700

    small edit to emailconfirmation.jsx

--- src/component/EmailConfirmation.jsx
+++ src/component/EmailConfirmation.jsx
@@ -5,7 +5,7 @@ class EmailConfirmation extends Component {
   render(){
     return(
       <div>
-        <h1 id="heading"> ENGAGE: Santa Monica, California </h1>
+        <h1 id="heading">ENGAGE: Santa Monica, California</h1>
         <div className="box">
           <h2 className="emailbox"> Thank You </h2>
           <h4 className="emailbox"> Your email is confirmed and your post has been submitted </h4>
