b887b779a80fb9a9e58c1e1c03bca3daef4d8859
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-07-11 02:09:06 -0700

    small stylistic edits to emailconfirmation.jsx

--- src/component/EmailConfirmation.jsx
+++ src/component/EmailConfirmation.jsx
@@ -5,12 +5,12 @@ class EmailConfirmation extends Component {
   render(){
     return(
       <div>
-        <h1 id="heading">ENGAGE: Santa Monica, California</h1>
+        <h1 id="heading"> ENGAGE: Santa Monica, California </h1>
         <div className="box">
-          <h2 className="emailbox">Thank You</h2>
-          <h4 className="emailbox">Your email is confirmed and your post has been submitted</h4>
+          <h2 className="emailbox"> Thank You </h2>
+          <h4 className="emailbox"> Your email is confirmed and your post has been submitted </h4>
           <br></br>
-        <a id="engagefeedlink"href="#"><h1>Go to the Engage Feed</h1></a>
+        <a id="engagefeedlink"href="#"><h1> Go to the Engage Feed </h1></a>
 
         </div>
       </div>
