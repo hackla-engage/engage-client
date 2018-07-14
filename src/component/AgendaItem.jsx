1660eab3f0d95a5820eeadcb8e109128810f6b6a
Jerry Lee <JerryDeKo@gmail.com>
2018-07-14 15:10:04 -0700

    Connected FormContainer to AgendaItem

--- src/component/AgendaItem.jsx
+++ src/component/AgendaItem.jsx
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import agenda_item_received from "../actions/Form";
+import { bindActionCreators } from "redux";
 
 //I can just get the id from the param and use to to fetch from Application state's agenda agendaitems
 //Now I just need to design a page and put informations on them
 import { Button, Card, Container } from "semantic-ui-react";
 
 class AgendaItem extends Component {
+  constructor() {
+    super();
+
+    this.recommendationReducer = this.recommendationReducer.bind(this);
+    this.summaryReducer = this.summaryReducer.bind(this);
+    this.showForm = this.showForm.bind(this);
+    this.goToForm = this.goToForm.bind(this);
+  }
+
   componentDidMount = () => {};
 
   timeConverter(UNIX_timestamp) {
@@ -47,13 +58,85 @@ class AgendaItem extends Component {
     return time;
   }
 
+  recommendationReducer = (acc, curr) => {
+    return acc + "<br />" + curr;
+  };
+
+  summaryReducer = (acc, curr) => {
+    if (!this.gotBackground) {
+      if (acc.toLowerCase().includes("summary")) {
+        return curr;
+      } else if (curr.toLowerCase().includes("background")) {
+        this.gotBackground = true;
+        return acc;
+      } else {
+        return acc + "<br />" + curr;
+      }
+    } else {
+      return acc;
+    }
+  };
+
+  showForm(proCon) {
+    const id = this.props.match.params.id;
+    const agenda = this.props.agendaItems[id];
+    const body = agenda.body;
+    const recommendations = agenda.recommendations[0].recommendation;
+    const title = agenda.title;
+
+    let recommendationsString = "";
+    // map to get recommendation from object and reduce array to string concatenated with <br />s
+    if (recommendations.length > 0) {
+      recommendationsString = recommendations
+        .map(rec => {
+          return rec;
+        })
+        .reduce(this.recommendationReducer);
+    }
+
+    let summaryString = "";
+    // slice and reduce array to string concatenated with <br />s
+    if (body.length > 0) {
+      summaryString = body.slice(0, 4).reduce(this.summaryReducer);
+    }
+    // Configure form content
+    this.props.agenda_item_received({
+      Title: title,
+      Recommendations: recommendationsString,
+      Summary: summaryString,
+      Id: id,
+      Pro: proCon === "pro"
+    });
+    setTimeout(this.goToForm, 200);
+  }
+
+  goToForm() {
+    this.props.history.push("/form");
+  }
+
   render() {
     const agendaItem = this.props.agendaItems[this.props.match.params.id];
 
     const agendaTime = this.timeConverter(agendaItem.meeting_time);
     const agendaBody = agendaItem.body;
     const agendaRecommendation = agendaItem.recommendations[0].recommendation;
-    console.log(agendaItem);
+
+    const body = agendaBody.map((string, index) => {
+      return <p key={index}>{string}</p>;
+    });
+
+    const recommendation = agendaRecommendation ? (
+      <div>
+        {agendaRecommendation.map((string, index) => {
+          return <p key={index}>{string}</p>;
+        })}
+      </div>
+    ) : (
+      <div>
+        <p>No recommended action has been proposed.</p>
+      </div>
+    );
+
     return (
       <Container style={{ margin: 24, color: "black" }}>
         <Card style={{ width: "auto" }}>
@@ -77,13 +160,11 @@ class AgendaItem extends Component {
           </Card.Content>
           <Card.Content>
             <Card.Header>BODY:</Card.Header>
-            {agendaBody.map((string, index) => {
-              return <p key={index}>{string}</p>;
-            })}
+            {body}
           </Card.Content>
           <Card.Content>
             <Card.Header>RECOMMENDED ACTION:</Card.Header>
-            {agendaRecommendation}
+            {recommendation}
           </Card.Content>
 
           <Card.Content>
@@ -91,10 +172,22 @@ class AgendaItem extends Component {
               WHAT IS YOUR POSITION ON THE RECOMMENDED ACTION?
             </Card.Header>
             <div className="ui three buttons" style={{ padding: 24 }}>
-              <Button basic color="green">
+              <Button
+                basic
+                color="green"
+                onClick={evt => {
+                  this.showForm("pro");
+                }}
+              >
                 Approve
               </Button>
-              <Button basic color="red">
+              <Button
+                basic
+                color="red"
+                onClick={evt => {
+                  this.showForm("con");
+                }}
+              >
                 Decline
               </Button>
               <Button basic color="black">
@@ -120,9 +213,11 @@ function mapStateToProps(state) {
   };
 }
 
-const mapDispatchToProps = {};
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators({ agenda_item_received }, dispatch);
+}
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  matchDispatchToProps
 )(AgendaItem);
--- src/container/AgendaFeedContainer.jsx
+++ src/container/AgendaFeedContainer.jsx
@@ -12,17 +12,12 @@ class AgendaFeed extends Component {
     super(props);
     this.addId = this.addId.bind(this);
     this.removeId = this.removeId.bind(this);
-    this.showForm = this.showForm.bind(this);
-    this.recommendationReducer = this.recommendationReducer.bind(this);
-    this.summaryReducer = this.summaryReducer.bind(this);
-    this.goToForm = this.goToForm.bind(this);
     this.getMoreAgendas = this.getMoreAgendas.bind(this);
   }
 
   componentDidMount() {
     // Kick off action to make async call to our server for tags/topics.
     // This will then get stored in our redux state.
-    console.log("ComponentDidMount");
     gtag("config", "UA-116538234-1", {
       page_title: "feed",
       page_path: "/feed"
@@ -46,59 +41,6 @@ class AgendaFeed extends Component {
     const parsed = qs.parse(this.props.location.search);
     delete parsed.id;
     this.props.history.push("/feed?" + qs.stringify(parsed));
-  }
-  recommendationReducer = (acc, curr) => {
-    return acc + "<br />" + curr;
-  };
-
-  summaryReducer = (acc, curr) => {
-    if (!this.gotBackground) {
-      if (acc.toLowerCase().includes("summary")) {
-        return curr;
-      } else if (curr.toLowerCase().includes("background")) {
-        this.gotBackground = true;
-        return acc;
-      } else {
-        return acc + "<br />" + curr;
-      }
-    } else {
-      return acc;
-    }
-  };
-  showForm(proCon) {
-    // this.requestedID is only useful at loading lifecycle event not afterward
-    const parsed = qs.parse(this.props.location.search);
-    if (parsed && parsed.id) {
-      const { id, body, recommendations, title } = this.props.agendaItems[
-        parsed.id
-      ];
-      let recommendationsString = "";
-      // map to get recommendation from object and reduce array to string concatenated with <br />s
-      if (recommendations.length > 0) {
-        recommendationsString = recommendations
-          .map(rec => {
-            return rec.recommendation;
-          })
-          .reduce(this.recommendationReducer);
-      }
-      let summaryString = "";
-      // slice and reduce array to string concatenated with <br />s
-      if (body.length > 0) {
-        summaryString = body.slice(0, 4).reduce(this.summaryReducer);
-      }
-      // Configure form content
-      this.props.agenda_item_received({
-        Title: title,
-        Recommendations: recommendationsString,
-        Summary: summaryString,
-        Id: id,
-        Pro: proCon === "pro"
-      });
-      setTimeout(this.goToForm, 200);
-    }
-  }
-  goToForm() {
-    this.props.history.push("/form"); // already set up!
   }
 
   getMoreAgendas() {
@@ -131,7 +73,6 @@ class AgendaFeed extends Component {
                 defaultOpen={agenda.id === this.requestedID}
                 removeId={this.removeId}
                 searchParams={this.props.location.search}
-                showForm={this.showForm}
               />
             );
           })}
